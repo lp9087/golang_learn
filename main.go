f190226ed339e8fe31ab6b101b32a6dd4bd1ee09
Kirill.Gibalov <http://gitlab.cosmos2.lan/>
2022-06-17 16:34:53 +0300

    golang learn

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+
+func main() {
+	var conferenceName = "Go Cinference"
+	const conferenceTickets = 50 
+	var remainingTickets = 50
+	bookings := []string{}
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets int
+
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter your number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+
+
+		if isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName + " " + lastName)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+
+			if  remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break 
+			}
+		} else {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+		}
+	}	
+
+}
+	
+
+	
